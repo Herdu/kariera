d10b65c837928dbe94880df1b493b901f9581bca
herdu <mateusz.dobrowolski71@gmail.com>
2017-03-07 14:32:14 +0100

    photos, map fix

--- js/script.js
+++ js/script.js
@@ -59,11 +59,35 @@ setInterval(displaySlajder,dlugosc_slajdu);
 
 
 
-var currentMap = 1;
+var currentMap;
+var oldMap;
+
+
+
+var tempChangeMap = function(){
+    console.log("yolo");
+    oldMap = currentMap;
+    currentMap = this.mapId;
+    changeMap();
+}
+
+
+var tempChangeMapOff = function(){
+    currentMap = oldMap;
+    changeMap();
+}
+
+var permChangeMap = function(){
+    currentMap = this.mapId;
+    oldMap = this.mapId;
+    changeMap();
+}
+
+
 
 var changeMap =  function(){
 
-    currentMap = this.mapId;
+
 
     $('#map-poznan').css("display","none");
     $('#map-poland').css("display","none");
@@ -71,19 +95,40 @@ var changeMap =  function(){
     console.log("current map = ",currentMap);
     switch(currentMap){
         case 1:
+
+
+
             $('#map-poznan').css("display","block");
+
+            $('#map-description').html("Nasz zasięg wczoraj");
             break;
         case 2:
             $('#map-poland').css("display","block");
+
+            $('#map-description').html("Nasz zasięg dziś");
             break;
         case 3:
             $('#map-europe').css("display","block");
+
+            $('#map-description').html("Nasz zasięg jutro");
             break;
     }
 };
 
-document.getElementById("wczoraj-button").mapId = 1;
-document.getElementById("dzis-button").mapId = 2;
-document.getElementById("jutro-button").mapId = 3;
 
-$('.map-button').on("mouseover",changeMap);
+$(document).ready(function(){
+    document.getElementById("wczoraj-button").mapId = 1;
+    document.getElementById("dzis-button").mapId = 2;
+    document.getElementById("jutro-button").mapId = 3;
+
+    $('.map-button').on("click",permChangeMap);
+
+
+    $('.map-button').on("mouseover",tempChangeMap);
+    $('.map-button').on("mouseleave",tempChangeMapOff);
+
+    currentMap = 2;
+    changeMap();
+});
+
+
