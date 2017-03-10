52e84d6faa3a9b32d283a0c2eff21cecdbe57c22
herdu <mateusz.dobrowolski71@gmail.com>
2017-03-10 11:52:36 +0100

    simple map

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
  * Created by mateu on 14.11.2016.
  */
 
-var dlugosc_slajdu = 6000;
+var dlugosc_slajdu = 7500;
 
 var slajder = document.getElementsByClassName("slajder");
 var currentSlajder = 0;
@@ -57,78 +57,28 @@ HTMLElement.prototype.removeClass = function(remove) {
 displaySlajder();
 setInterval(displaySlajder,dlugosc_slajdu);
 
+/** MAP interval
+ **/
+var dlugosc_mapy = 2000;
+var currentMap = 1;
 
-
-var currentMap;
-var oldMap;
-
-
-
-var tempChangeMap = function(){
-    console.log("yolo");
-    oldMap = currentMap;
-    currentMap = this.mapId;
-    changeMap();
-}
-
-
-var tempChangeMapOff = function(){
-    currentMap = oldMap;
-    changeMap();
-}
-
-var permChangeMap = function(){
-    currentMap = this.mapId;
-    oldMap = this.mapId;
-    changeMap();
-}
-
-
-
-var changeMap =  function(){
-
-
-
-    $('#map-poznan').css("display","none");
-    $('#map-poland').css("display","none");
-    $('#map-europe').css("display","none");
-    console.log("current map = ",currentMap);
-    switch(currentMap){
+var displayMap = function(){
+    currentMap = (currentMap%3)+1;
+    console.log(currentMap);
+    switch (currentMap){
         case 1:
-
-
-
-            $('#map-poznan').css("display","block");
-
-            $('#map-description').html("Nasz zasięg wczoraj");
+            $('#our-map').attr("class","yesterday");
+            $("#map-description").html("Wczoraj");
             break;
         case 2:
-            $('#map-poland').css("display","block");
-
-            $('#map-description').html("Nasz zasięg dziś");
+            $('#our-map').attr("class","today");
+            $("#map-description").html("Dziś");
             break;
         case 3:
-            $('#map-europe').css("display","block");
-
-            $('#map-description').html("Nasz zasięg jutro");
+            $('#our-map').attr("class","tomorrow");
+            $("#map-description").html("Jutro");
             break;
     }
-};
+}
 
-
-$(document).ready(function(){
-    document.getElementById("wczoraj-button").mapId = 1;
-    document.getElementById("dzis-button").mapId = 2;
-    document.getElementById("jutro-button").mapId = 3;
-
-    $('.map-button').on("click",permChangeMap);
-
-
-    $('.map-button').on("mouseover",tempChangeMap);
-    $('.map-button').on("mouseleave",tempChangeMapOff);
-
-    currentMap = 2;
-    changeMap();
-});
-
-
+setInterval(displayMap,dlugosc_mapy);
